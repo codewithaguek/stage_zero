d6dfb9741b75f3bb6485489bcae2676595f2ee42
Anyieth Aguek <makueireng98@gmail.com>
2025-01-29 20:31:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from datetime import datetime
+import pytz
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=['*'],
+allow_methods=['*'],
+allow_headers=['*'],
+allow_credentials=True
+	)
+
+
+@app.get("/")
+
+def def_root():
+	return {
+	"email": "makueireng98@gmail.com",
+	"curr_datetime": datetime.now(pytz.utc).isoformat(),
+	"github_link": "https://github.com/Aguek/stage_zero"
+	}
